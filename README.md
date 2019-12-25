7de351ef6ef70b588a372250bd17eebe2cfca2a6
CCThomas <ccthom94@gmail.com>
2019-12-25 15:15:55 -0600

    hello-world

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Hidden-Role-To-Go
 Hidden Role Web App
+
+## Run in Terminal
+mvn spring-boot:run
---
+++ src/test/java/com/github/virtualgamers/hrtg/HiddenRoleToGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.virtualgamers.hrtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiddenRoleToGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
